76db828dda3a83b8404107b00f4a1fad7713a8dc
agmo <mor.agnieszka@gmail.com>
2016-01-19 21:42:36 +0100

    Move the favouriteStops object to a separate file

---
+++ scripts/favouriteStops.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Agnieszka on 2016-01-19.
+ */
+var favouriteStops = {
+    favouriteStop1: 'Migowo',
+    favouriteStop2: 'Dworzec Główny',
+    favouriteStop3: 'Żabi Kruk'
+};
--- scripts/js-pickYourStops-localStorage.js
+++ scripts/js-pickYourStops-localStorage.js
@@ -1,12 +1,6 @@
 /**
  * Created by Agnieszka on 2016-01-19.
  */
-var favouriteStops = {
-    favouriteStop1: 'Migowo',
-    favouriteStop2: 'Dworzec Główny',
-    favouriteStop3: 'Żabi Kruk'
-};
-
 $(function () {
     var $pickYourStopsContainer = $('#js-pickYourStops');
 
