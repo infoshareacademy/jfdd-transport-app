b1eb7641c21337e83990490c191faa9437bbea59
agmo <mor.agnieszka@gmail.com>
2016-01-20 16:56:31 +0100

    Modify project structure

---
+++ src/modules/pickYourStops/model/user.js
@@ -0,0 +1,9 @@
+(function() {
+    pickYourStops = pickYourStops || {};
+    pickYourStops.model = pickYourStops.model || {};
+    pickYourStops.model.user = {
+        fetch: function() {}
+    };
+
+
+}());
