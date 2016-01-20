8b1794e8dbb8b4bab825103f763395ee4428c6e4
agaSemrau <agnieszka.semrau@wp.pl>
2016-01-20 23:15:13 +0100

    login with google+ popup

--- src/modules/login/main.js
+++ src/modules/login/main.js
@@ -6,19 +6,20 @@ var popup = document.createElement('div');
 var close = $('<span>x</span>');
     (close).appendTo(popup);
 
+function quit(){
+    window.location.href='http://localhost:63342/jfdd-transport/index.html';
+}
+
+$(close).click(quit);
 
 function onSuccess(googleUser) {
     console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-    $('.popup').remove();
+    $('.popup').hide();
 }
 function onFailure(error) {
     console.log(error);
 }
 
-//function popup() {
-//    var $myDiv = $('#my-signin2');
-//    $('<p>Zaloguj się przez</p>').appendTo("#my-signin2");
-//}
 function renderButton() {
     gapi.signin2.render('my-signin2', {
         'scope': 'https://www.googleapis.com/auth/plus.login',
@@ -32,38 +33,4 @@ function renderButton() {
 }
 
 $("#my-signin2").appendTo(popup);
-
-
-
-
-
-//(function(){
-//
-//    var $myDiv = $('#my-signin2');
-    //$('<p>Zaloguj się przez</p>').appendTo("#my-signin2");
-//
-//}());
-
-
-//(function(){
-
-    //login = login || {};
-
-    //login.main = {
-    //    init: function () {
-    //        var popup = document.createElement ('div');
-    //        var popupText = document.createTextNode("Zaloguj się przez:");
-    //        popup.appendChild(popupText);
-    //        document.body.appendChild(popup);
-        //}
-
-        //kaboom: function () {
-        //
-        //}
-    //};
-
-    //login.main.init();4
-
-
-//}());
 
