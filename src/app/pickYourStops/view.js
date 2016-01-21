884a50ed300df24665a426cd9ebc1655a9dd00dd
anna <annokr1@gmail.com>
2016-01-21 14:33:49 +0100

    changes structure project

--- src/app/pickYourStops/view.js
+++ src/app/pickYourStops/view.js
@@ -1,5 +1,5 @@
 //dodawanie nazw przystanków do listy
-ns('src.app.pickYourStops', getBusStops(function (busStops) {
+ns('app.pickYourStops.view', getBusStops(function (busStops) {
     var html = '<input list="browsers" name="browser" /><datalist id="browsers" />'
     $('#js-pickYourStops').append(html);
     busStops.forEach(function (busStop) {
