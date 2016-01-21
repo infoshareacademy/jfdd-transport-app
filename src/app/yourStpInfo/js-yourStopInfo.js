62108cd1e3573fec34763a13cd563729b82c5ade
Agnieszka Semrau <agnieszka.semrau@wp.pl>
2016-01-21 15:47:02 +0100

    FDT 69 ns

--- src/app/yourStpInfo/js-yourStopInfo.js
+++ src/app/yourStpInfo/js-yourStopInfo.js
@@ -1,13 +1,14 @@
-/**
- * Created by agasemrau on 19.01.16.
- */
-$(document).ready(function(){
+ns('app.login.main', function () {
+
+   function showDiv (){
     var $jsyourStopInfo = $('#js-yourStopInfo');
 
     $jsyourStopInfo.append('<h1>Twoje Przystanki</h1>');
     $jsyourStopInfo.append('<h3 class="stop1">Przystanek 1</h3>');
     $jsyourStopInfo.append('<h3 class="stop2">Przystanek 2</h3>');
     $jsyourStopInfo.append('<h3 class="stop3">Przystanek 3</h3>');
+   }
+
 
 
 });
