6ee2a8d987af7a632f379ff4d228a0a7fd39e8cd
agaSemrau <agnieszka.semrau@wp.pl>
2016-01-21 21:34:52 +0100

    json does not work

--- src/app/yourStopInfo/main.js
+++ src/app/yourStopInfo/main.js
@@ -3,15 +3,35 @@ ns('app.yourStopInfo.main', function () {
    function showDiv (){
     var $jsyourStopInfo = $('#js-yourStopInfo');
 
-    $jsyourStopInfo.append('<h1>Twoje Przystanki</h1>');
-    $jsyourStopInfo.append('<h3 class="stop1">Przystanek 1</h3>');
-    $jsyourStopInfo.append('<h3 class="stop2">Przystanek 2</h3>');
-    $jsyourStopInfo.append('<h3 class="stop3">Przystanek 3</h3>');
+    $jsyourStopInfo.append('<h1>Twoje przystanki: </h1>');
+    $jsyourStopInfo.append('<div class="stop1"><h3>Przystanek 1</h3></div>');
+    $jsyourStopInfo.append('<div class="stop2"><h3>Przystanek 2</h3></div>');
+    $jsyourStopInfo.append('<div class="stop3"><h3>Przystanek 3</h3></div>');
    }
+
+
+    function getJasonData() {
+        $.ajax({
+            url: ('http://jsonplaceholder.typicode.com/posts'),
+            dataType: 'json',
+            error: console.log('error'),
+            success: function(response){
+                var kontener = $('.stop1');
+                response.forEach(function(title) {
+                    kontener.append('<p>title</p>')
+
+                })
+            }
+        })
+    }
+
+
+
 
 return {
     init: function() {
         showDiv();
+        getJasonData();
 
     }
 }
