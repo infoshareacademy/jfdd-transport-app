7f80bfa217b6d2c3dc3595934060e8efa7e20079
agaSemrau <agnieszka.semrau@wp.pl>
2016-01-21 23:20:31 +0100

    json works

--- src/app/yourStopInfo/main.js
+++ src/app/yourStopInfo/main.js
@@ -4,38 +4,59 @@ ns('app.yourStopInfo.main', function () {
         var $jsyourStopInfo = $('#js-yourStopInfo');
 
         $jsyourStopInfo.append('<h1>Twoje przystanki: </h1>');
-        $jsyourStopInfo.append('<div class="stop1"><h3>Przystanek 1</h3></div>');
-        $jsyourStopInfo.append('<div class="stop2"><h3>Przystanek 2</h3></div>');
-        $jsyourStopInfo.append('<div class="stop3"><h3>Przystanek 3</h3></div>');
+        $jsyourStopInfo.append('<div class="stop1"><h3>Przystanek 1<p class = "fetchingStatus"></p></h3></div>');
+        $jsyourStopInfo.append('<div class="stop2"><h3>Przystanek 2<p class = "fetchingStatus"></p></h3></div>');
+        $jsyourStopInfo.append('<div class="stop3"><h3>Przystanek 3<p class = "fetchingStatus"></p></h3></div>');
     }
 
-    var dataFromJson = [];
+    var result1=[];
+    var result2=[];
+
 
     function getJsonData() {
+
+        var $fetchingStatus = $('.fetchingStatus');
+        $fetchingStatus.text('Pobieranie...');
+
         $.ajax({
             type: "GET",
             dataType: 'jsonp',
-            data: { get_param: 'value' },
+            data: {},
             url: 'http://jsonplaceholder.typicode.com/posts',
+
             success: function (data) {
+                $fetchingStatus.hide();
                 console.log(data);
-                var $kontener = $('.stop1');
-                data.forEach(function (val) {
-                    $kontener.append($('<p>', {
-                        value: val.title
-                    }).text(val.title))
+                var $kontener1 = $('.stop1');
+                var $kontener2= $('.stop2');
+                data.forEach(function (items) {
+
+                    result1 = ($('<p>', {
+                        value: items.title
+                    }).text(items.title));
+
+                   result2 = ($('<p>', {
+                        value: items.id
+                    }).text(items.id));
+
+                    $kontener1.append(result1);
+                    $kontener2.append(result2)
                 });
             }
         });
 
     }
 
-
+function fillIn () {
+    var $kontener3 = $('.stop3');
+    $kontener3.append(result1);
+}
 
 return {
     init: function() {
         showDiv();
         getJsonData();
+        fillIn();
     }
 }
 
