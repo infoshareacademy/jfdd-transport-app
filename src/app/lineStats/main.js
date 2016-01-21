ea38f75096fa33d5c5bfa006da30c267df53518e
wprzebieracz <przebieracz@gmail.com>
2016-01-21 16:12:16 +0100

    add data fetching by ajax

--- src/app/lineStats/main.js
+++ src/app/lineStats/main.js
@@ -2,6 +2,27 @@ ns('app.lineStats.main', function () {
     return {
         init : function () {
             $('<p>Dupa</p>').appendTo('#js-lineStats');
+
+            $('#js-lineStats').click(function () {
+
+                $.ajax({
+                    type: "GET",
+                    dataType: "jsonp",
+                    url: "http://jsonplaceholder.typicode.com/users",
+                    success: function(data){
+                        console.log(data);
+                        $select = $('<select/>');
+                        data.forEach(function (row) {
+                            $select.prepend($('<option>', {
+                                value : row.name
+                            }).text(row.name))
+                        });
+                        $('#js-lineStats').prepend($select);
+
+                    }
+                });
+
+            });
         }
     };
 });
