55462894cb56f8b3d946b36be59ae7d3ba97fdde
anna <annokr1@gmail.com>
2016-01-22 16:54:44 +0100

    Add button event

--- src/app/pickYourStops/view.js
+++ src/app/pickYourStops/view.js
@@ -1,20 +1,35 @@
 //dodawanie nazw przystanków do listy
 ns('app.pickYourStops.view', function () {
-    return {
-        init: function (busStops) {
-            $('#js-pickYourStops')
-                .append($('<input list="stops">').append($('<datalist id="stops">')
-                    .append(
-                        busStops.map(
-                            function (busStop) {
-                                return $('<option>').attr('value', busStop.name);
-                            }
-                        )
-                    ))
-                )
-                .append($('<button>').text("Wybierz"));
+        return {
+            init: function (busStops) {
+                $('#js-pickYourStops')
+                    .append($('<input list="stops">').append($('<datalist id="stops">')
+                        .append(
+                            busStops.map(
+                                function (busStop) {
+                                    return $('<option>').attr('value', busStop.name);
+                                }
+                            )
+                        ))
+                    )
+                    .append($('<button id="myBtn">').text("Wybierz")).append($('<div class="selectedStop">'));
+                /*document.getElementById("myBtn").onclick = displayStop;
+                 function displayStop() {
+                 document.getElementById("selectedStop").innerHTML = "przystanek";*/
+                $('#myBtn').on('click', function () {
+
+                        $('.selectedStop').append(
+                            '<div>' + $('#js-pickYourStops input').val() + '</div>'
+                        );
+
+                        //busStops.map(
+                        //    function (busStop) {
+                        //        return $('.selectedStop').text(busStop.name);
+                        //    })
+                    }
+                );
+            }
         }
     }
-
-});
+);
 
