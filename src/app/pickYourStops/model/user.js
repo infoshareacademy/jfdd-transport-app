bd1b0c20c3b156b62a2493bac3eb899fba18e09a
Agnieszka Semrau <agnieszka.semrau@wp.pl>
2016-01-22 16:59:58 +0100

    div you stops is getting stops name from localhost

--- src/app/pickYourStops/model/user.js
+++ src/app/pickYourStops/model/user.js
@@ -13,7 +13,7 @@ ns('app.pickYourStops.model.user', function() {
     };
 
     var getStops = function() {
-        return ['Migowo', 'Dworzec Główny', 'Żabi Kruk'];
+        return ['Migowo', 'Dworzec Główny', 'Żabi Kruk', 'Przymorze', 'Suchanino'];
     };
 
     return {
--- src/app/yourStopInfo/main.js
+++ src/app/yourStopInfo/main.js
@@ -2,13 +2,12 @@ ns('app.yourStopInfo.main', function () {
 
     function showDiv() {
         var $jsyourStopInfo = $('#js-yourStopInfo');
-
         var tablicaPrzystanków = app.pickYourStops.model.user.favouriteStops();
 
 
         tablicaPrzystanków.forEach(function(przystanek) {
             $jsyourStopInfo.append(
-                '<div class="stop1"><h3>' + przystanek + '<p class = "fetchingStatus"></p></h3></div>'
+                '<div class="yourStop"><h3>' + przystanek + '<p class = "fetchingStatus"></p></h3></div>'
             )
         });
 
