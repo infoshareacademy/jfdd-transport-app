b827c3e369340939f5c5b0629413c13d9e3d496b
anna <annokr1@gmail.com>
2016-01-25 16:00:17 +0100

    FDT-77-rename button id

--- src/app/pickYourStops/view.js
+++ src/app/pickYourStops/view.js
@@ -12,7 +12,7 @@ ns('app.pickYourStops.view', function () {
                             )
                         ))
                     )
-                    .append($('<button id="myBtn">').text("Wybierz")).append($('<div class="selectedStop">'));
+                    .append($('<button id="pickStop">').text("Wybierz")).append($('<div class="selectedStop">'));
 
                 $('#myBtn').on('click', function () {
 
