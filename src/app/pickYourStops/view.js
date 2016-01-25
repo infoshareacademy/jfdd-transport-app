d3157adc01881fd780c132690f772b1c953b5d15
annokr <annokr1@gmail.com>
2016-01-25 22:27:06 +0100

    Add button 'usuń'; Add new api; Prevent adding empty busStops.

--- src/app/pickYourStops/view.js
+++ src/app/pickYourStops/view.js
@@ -7,7 +7,7 @@ ns('app.pickYourStops.view', function () {
                         .append(
                             busStops.map(
                                 function (busStop) {
-                                    return $('<option>').attr('value', busStop.name);
+                                    return $('<option>').attr('value', busStop.id);
                                 }
                             )
                         ))
@@ -15,12 +15,19 @@ ns('app.pickYourStops.view', function () {
                     .append($('<button type="button" id="pickStop">').text("Wybierz")).append($('<div class="selectedStop">'));
 
                 $('#pickStop').on('click', function () {
+                        var inputList = $('#js-pickYourStops input[list="stops"]');
+                        if (inputList.val()) {
+                            // logika zapisywania do local storage
+                            // jak wywolac funkcje modelu user albo busStops?
+                            // np. busStops.saveBusStop();
 
-                        $('.selectedStop').append(
-                            '<div>' + $('#js-pickYourStops input[list="stops"]').val() + '</div>'
-                        );
+                            // dodatkowo przycisk z usuwaniem wybranego elementu, rowniez trzeba by wywolac funckje typu removeUsersBusStop() user model z id przystanku
+                            $('.selectedStop').append(
+                                '<div><span>' + inputList.val() + '</span><button onclick=" + userModelCostam.removeUsersBusStop(inputList.val()) + ">usuń</button></div>'
+                            );
 
-                        $('#js-pickYourStops input[list="stops"]').val('');
+                            inputList.val('');
+                        }
                     }
                 );
             }
@@ -28,3 +35,4 @@ ns('app.pickYourStops.view', function () {
     }
 );
 
+//
