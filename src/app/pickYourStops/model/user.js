2198bddaec6d7b744f8a23f6df602a7aca7e4f54
agmo <mor.agnieszka@gmail.com>
2016-01-25 21:49:37 +0100

    Add test for updating local storage

--- src/app/pickYourStops/model/user.js
+++ src/app/pickYourStops/model/user.js
@@ -1,30 +1,33 @@
-ns('app.pickYourStops.model.user', function() {
+ns('app.pickYourStops.model.user', function () {
 
-    var usernameInStorage = function(username) {
+    var usernameInStorage = function (username) {
         return localStorage.getItem(username);
     };
 
-    var displayUserStops = function(username){
+    var displayUserStops = function (username) {
         return JSON.parse(localStorage.getItem[username]);
     };
 
-    var updateStorage = function(username, stops) {
-        localStorage.setItem(username, JSON.stringify(stops));
+    var updateStorage = function (username, stop) {
+        var stopsArray = JSON.parse(localStorage.getItem(username)) || [];
+        stopsArray.push(stop);
+        localStorage.setItem(username, JSON.stringify(stopsArray));
     };
 
-    var getStops = function() {
+    var getStops = function () {
         return ['Migowo', 'Dworzec Główny', 'Żabi Kruk', 'Przymorze', 'Suchanino'];
     };
 
     return {
 
-      init: function (username) {
-          if(usernameInStorage()) {
-              console.log(displayUserStops(usernameInStorage(username)));
-          } else {
-              updateStorage(username, getStops());
-          }
-      },
-      favouriteStops: getStops
+        init: function (username) {
+            if (usernameInStorage()) {
+                console.log(displayUserStops(usernameInStorage(username)));
+            } else {
+                updateStorage(username, getStops());
+            }
+        },
+        favouriteStops: getStops,
+        addToFavouriteStops: updateStorage
     };
 });
