0e23ab8233c77187d2c1e91132b7d7e60316a02a
agmo <mor.agnieszka@gmail.com>
2016-01-26 16:10:42 +0100

    Load a list of lines from API

--- src/app/lineStats/view.js
+++ src/app/lineStats/view.js
@@ -3,9 +3,18 @@
  */
 ns('app.lineStats.view', function () {
 
+    var getLineList = function(lines) {
+        var $datalistEl = $('#lines');
+
+        $datalistEl.append(lines.map(function(line) {
+            return $('<option>').attr('value', line.id);
+        }));
+
+    };
+
     return {
         init: function () {
-
+            app.dataManager.fetch('https://isa-api.herokuapp.com/transport/lines.json', [getLineList]);
         }
     }
 });
