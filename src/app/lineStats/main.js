60aa619d183cc6529e8e7eaa89e008e0003adb90
agmo <mor.agnieszka@gmail.com>
2016-01-26 17:04:07 +0100

    Fix a bug when clicking the choose button

--- src/app/lineStats/main.js
+++ src/app/lineStats/main.js
@@ -2,8 +2,8 @@
  * Created by agmo on 22.01.16.
  */
 ns('app.lineStats.main', function () {
-    var displayStats = function () {
-        console.log('stats');
+    var displayStats = function (stats) {
+        console.log(stats);
     };
 
     return {
@@ -18,16 +18,16 @@ ns('app.lineStats.main', function () {
                     $('#selectedLines').append('<div>' + lineList.val() + '</div>');
 
                     currentLines.push(lineList.val());
+
+                    if ($('#showStats').length < 1) {
+                        $('#js-lineStats').append($('<button id="showStats" type="button">' + 'Wyświetl statystyki' + '</button>'));
+                    }
                 }
                 lineList.val('');
-
-                if ($('#showStats').length < 1) {
-                    $('#js-lineStats').append($('<button id="showStats" type="button">' + 'Wyświetl statystyki' + '</button>'));
-                }
             });
 
             $('#js-lineStats').on('click', '#showStats', function () {
-                displayStats();
+                app.dataManager.fetch('https://isa-api.herokuapp.com/transport/lines.json', [displayStats]);
             });
         }
     };
