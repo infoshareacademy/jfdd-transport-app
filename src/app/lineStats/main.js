4259f8386e6abf2a0c1a0b71f006ed070fdd6343
agmo <mor.agnieszka@gmail.com>
2016-01-26 16:43:54 +0100

    Display lines chosen by user

--- src/app/lineStats/main.js
+++ src/app/lineStats/main.js
@@ -4,8 +4,15 @@
 ns('app.lineStats.main', function () {
 
     return {
-      init: function() {
-        app.lineStats.view.init();
-      }
+        init: function () {
+            app.lineStats.view.init();
+            $('#chooseLines').on('click', function () {
+                var lineList = $('#js-lineStats input[list="lines"]');
+                if (lineList.val()) {
+                    $('#selectedLines').append('<div>' + lineList.val() + '</div>');
+                }
+                lineList.val('');
+            });
+        }
     };
 });
