1199937941479e109b285738cfbb378e076435ab
agmo <mor.agnieszka@gmail.com>
2016-01-26 18:37:31 +0100

    Refactor the user init function

--- src/app/pickYourStops/model/user.js
+++ src/app/pickYourStops/model/user.js
@@ -1,10 +1,6 @@
 ns('app.pickYourStops.model.user', function () {
     var currentUser = '';
 
-    var usernameInStorage = function (username) {
-        return localStorage.getItem(username);
-    };
-
     var displayUserStops = function (username) {
         return JSON.parse(localStorage.getItem[username]);
     };
@@ -16,9 +12,7 @@ ns('app.pickYourStops.model.user', function () {
     };
 
     var getStops = function () {
-        var userStops = JSON.parse(localStorage.getItem(currentUser));
-        console.log(userStops + ' ' + typeof userStops);
-        return userStops;
+        return JSON.parse(localStorage.getItem(currentUser)) || [];
     };
 
     var removeFromStorage = function () {
@@ -29,11 +23,6 @@ ns('app.pickYourStops.model.user', function () {
 
         init: function (username) {
             currentUser = username;
-            if (usernameInStorage()) {
-                console.log(displayUserStops(usernameInStorage(username)));
-            } else {
-                updateStorage('Migowo');
-            }
         },
         favouriteStops: getStops,
         addToFavouriteStops: updateStorage,
