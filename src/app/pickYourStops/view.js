f3ffca4825abacfb0f5667833677e51fcebb7e20
agmo <mor.agnieszka@gmail.com>
2016-01-26 18:00:02 +0100

    Update storage with busstops from API

--- src/app/pickYourStops/view.js
+++ src/app/pickYourStops/view.js
@@ -26,8 +26,8 @@ ns('app.pickYourStops.view', function () {
                                 )
                             );
 
+                            app.pickYourStops.model.user.addToFavouriteStops(inputList.val());
                             inputList.val('');
-                            app.pickYourStops.model.user.addToFavouriteStops(inputList);
                         }
                     }
 
