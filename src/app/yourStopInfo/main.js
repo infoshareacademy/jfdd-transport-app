920cc80bfb5e885f2ba5e59ee63b7634512c88e9
wprzebieracz <przebieracz@gmail.com>
2016-01-29 12:26:04 +0100

    add timetables, extract functionality to sepaarate file

--- src/app/yourStopInfo/main.js
+++ src/app/yourStopInfo/main.js
@@ -29,56 +29,11 @@ ns('app.yourStopInfo.main', function () {
             )
         });
         var stopsFromLocalStorageArray = app.pickYourStops.model.user.favouriteStops();
-
-
-        app.dataManager.fetch('https://isa-api.herokuapp.com/transport/lines.json', [
-            function (jsonWithPublicTransportLines) {
-                stopsFromLocalStorageArray.forEach(function (singleStopNameFromLocalStorageArray) {
-                    var $singleStopTable = $('<table class="table">');
-                    var tr = $('<tr>');
-                    var $tableHeading = $('<th>').append(singleStopNameFromLocalStorageArray);
-                    $singleStopTable.append(tr.append($tableHeading));
-
-                    var linesContainingStopFromLocalStorageArray = jsonWithPublicTransportLines.filter(function (singlePublicTransportLine) {
-                        return singlePublicTransportLine.stops.find(function (publicTransportStop) {
-                                return publicTransportStop.name === singleStopNameFromLocalStorageArray;
-                            }) !== undefined; // why check against undefined?
-                    });
-                    console.log(linesContainingStopFromLocalStorageArray.length);
-
-                    linesContainingStopFromLocalStorageArray.forEach(function (singleLine) {
-                        var lineNumber =singleLine.id;
-                        var $lineNumberRow = $('<tr>');
-                        var $lineNumberCell = $('<td>').append(lineNumber);
-                        $lineNumberRow.append($lineNumberCell);
-
-                        var matchingStopFromJsonAgainstLocalstorage = singleLine.stops.find(function (singleStop) {
-                            return singleStop.name === singleStopNameFromLocalStorageArray;
-                        });
-                        var matchingStopArrayIndex = singleLine.stops.indexOf(matchingStopFromJsonAgainstLocalstorage);
-                        console.log(matchingStopArrayIndex);
-                        var time = singleLine.dTimes.slice(0, matchingStopArrayIndex + 1).reduce(function (a, b) {
-                            return a + b;
-                        }, 0);
-                        console.log(time);
-
-                        var $departuresCell = $('<td id = "departureTimes">');
-
-                        var singleLineDepartures = singleLine.departures.forEach(function (sigleDepartureTime) {
-                            var depTimesInSeconds = sigleDepartureTime.hour * 3600 + sigleDepartureTime.minutes * 60 + sigleDepartureTime.seconds;
-                            var depTimeOnCurrentStop = (depTimesInSeconds + time) % 86400; //?
-                            var depTimeOnCurrentStopHHMMSS = toHHMMSS(depTimeOnCurrentStop);
-                            $departuresCell.append(" " + depTimeOnCurrentStopHHMMSS + " ");
-                        });
-
-                        $lineNumberRow.append($departuresCell);
-
-                        $singleStopTable.append($lineNumberRow);
-                    });
-                    $jsyourStopInfo.append($singleStopTable);
-                });
-            }
-        ]);
+        //
+        //
+        //app.dataManager.fetch('https://isa-api.herokuapp.com/transport/lines.json', [
+        //
+        //]);
 
 
     }
