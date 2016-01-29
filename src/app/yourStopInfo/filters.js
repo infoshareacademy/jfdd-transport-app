6cf5e4fa9135d45d7bf93cccdb2045d2923b3728
agaSemrau <agnieszka.semrau@wp.pl>
2016-01-30 00:30:39 +0100

    filter one returns true/false - but elements do not append

--- src/app/yourStopInfo/filters.js
+++ src/app/yourStopInfo/filters.js
@@ -10,81 +10,31 @@ ns('app.yourStopInfo.filters', function () {
 //Filter Departure time <5 minutes:----------------------------------------
     function filterOne(stopName) {
 
+        var actualTime = new Date();
+        var hours = actualTime.getHours() * 3600;
+        var minutes = actualTime.getMinutes() * 60;
+        var secs = actualTime.getSeconds();
 
-        //var actualTime = new Date();
-        //var hours = actualTime.getHours();
-        //var minutes = actualTime.getMinutes();
-        //var seconds = actualTime.getSeconds();
-        //
-        //if (hours < 10) {
-        //    hours = "0" + hours;
-        //}
-        //if (minutes < 10) {
-        //    minutes = "0" + minutes;
-        //}
-        //if (seconds < 10) {
-        //    seconds = "0" + seconds;
-        //}
-        //var actualHour = hours + ':' + minutes + ':' + seconds;
-        //
-        //console.log(actualHour);
-
-
-
+        var seconds = hours + minutes + secs;
+        //console.log('test');
 
         var departureTimes = app.yourStopInfo.timetable.timetables;
-        console.log(departureTimes);
+        //console.log(departureTimes[stopName]);
 
+        departureTimes[stopName].forEach(function (stopName) {
+            stopName.departures.forEach(function (departures) {
+                //console.log(departures.pure-seconds);
 
-        //console.log(actualHour);
-
-        var actualHour = new Date();
-        var seconds = actualHour.getSeconds();
-
-        for(departures in departureTimes[stopName]){
-               for(pure in departures){
-                   if(pure-seconds<300){
-                       return true
-                   }
-               }
-
-        }
-
-        //console.log(departureTimes[stopName][0].departures[0].pure);
-
-        //var lineTime = departureTimes[stopName][0].departures[0].pure;
-        //
-        //if(lineTime - seconds<300){return true;}
-
-
-
-
-        //var $departuresTextValue = departureTimes.text();
-        //
-        //console.log($departuresTextValue);
-        //var departureValuesSplit = $departuresTextValue.split(" ");
-        //console.log(departureValuesSplit);
-        //
-        //var fixDepartureValuesSplit = [];
-        //for(var i=1; i<departureValuesSplit.length; i=i+2){
-        //    fixDepartureValuesSplit.push(departureValuesSplit[i]);
-        //}
-
-
-        //console.log(fixDepartureValuesSplit);
-       //
-       //var hourToSeconds = actualHour.split(':');
-       //  var actualSeconds = (+hourToSeconds[0]) * 60 * 60 + (+hourToSeconds[1]) * 60 + (+hourToSeconds[2])
-       //
-       // fixDepartureValuesSplit.forEach(function (time) {
-       //     var linesHourToSeconds = time.split(':');
-       //     var linesTimeInSeconds = (+linesHourToSeconds[0]) * 60 * 60 + (+linesHourToSeconds[1]) * 60 + (+linesHourToSeconds[2])
-       //     var wynik = actualSeconds - linesTimeInSeconds;
-       //     return wynik;
-       //     console.log(wynik);
-       //
-       //
-       //     });
+                if (departures.pure-seconds <= 1000) {
+                    //return true;
+                console.log('true')
+                }
+                //else {
+                //console.log('false')
+                //    return false;
+                //}
+            })
+        });
     }
 
 
@@ -99,10 +49,9 @@ ns('app.yourStopInfo.filters', function () {
                     return stop.name === stopName;
                 }) !== undefined;
         });
-        console.log(filteredLines);
 
         var accumulator = [];
-
+console.log(filteredLines);
 
         filteredLines.forEach(function (line) {
             line.stops.forEach(function (stop) {
@@ -111,7 +60,6 @@ ns('app.yourStopInfo.filters', function () {
                 }
             });
         });
-        console.log(accumulator);
 
         if (accumulator.length >= 3) {
             return true;
