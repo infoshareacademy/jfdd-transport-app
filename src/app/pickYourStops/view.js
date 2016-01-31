d17280215955900c67b48aaa59f97973236ace2a
Wojciech Przebieracz <przebieracz@gmail.com>
2016-01-31 17:34:23 +0100

    display departurews of newly added stops without the need for page refresh

--- src/app/pickYourStops/view.js
+++ src/app/pickYourStops/view.js
@@ -36,6 +36,7 @@ ns('app.pickYourStops.view', function () {
                     var selectedBusStop = inputList.val();
                     if (selectedBusStop) {
                         app.pickYourStops.model.user.addToFavouriteStops(selectedBusStop);
+                        app.state.init();
                         app.yourStopInfo.main.refresh();
                         inputList.val('');
                     }
