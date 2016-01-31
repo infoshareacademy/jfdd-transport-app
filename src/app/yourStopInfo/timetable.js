2383c8903c2bc73e15eafafe32a5bec4f5a4aa25
Wojciech Przebieracz <przebieracz@gmail.com>
2016-01-31 17:25:16 +0100

    change returned time format from HH:MM:SS to HH:MM

--- src/app/yourStopInfo/timetable.js
+++ src/app/yourStopInfo/timetable.js
@@ -6,7 +6,6 @@ ns('app.yourStopInfo.timetable', function () {
         var sec_num = parseInt(seconds_parameter, 10);  // don't forget the radix in the second param
         var hours = Math.floor(sec_num / 3600);
         var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-        var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
         if (hours < 10) {
             hours = "0" + hours;
@@ -14,10 +13,7 @@ ns('app.yourStopInfo.timetable', function () {
         if (minutes < 10) {
             minutes = "0" + minutes;
         }
-        if (seconds < 10) {
-            seconds = "0" + seconds;
-        }
-        var time = hours + ':' + minutes + ':' + seconds;
+        var time = hours + ':' + minutes;
         return time;
     };
 
@@ -77,7 +73,7 @@ ns('app.yourStopInfo.timetable', function () {
                 });
             });
         });
-        //app.yourStopInfo.main.refresh();
+        app.yourStopInfo.main.refresh();
     };
 
     return {
