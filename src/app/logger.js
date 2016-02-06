0d67f6f1080f2fb92be0648f5d9a47ac4849dee5
agaSemrau <agnieszka.semrau@wp.pl>
2016-02-06 22:27:40 +0100

    add getLog to logger

--- src/app/logger.js
+++ src/app/logger.js
@@ -4,11 +4,13 @@ ns('app.logger', function () {
 
     var log = function(event){
         events.push(event);
-    console.log(event);
+        console.log(event);
         localStorage.setItem("Logger",JSON.stringify(events))
     };
 
-
+    var getLog = function(event){
+        return JSON.parse(localStorage.getItem("Logger")) || {};
+    };
     return {
         log:log
     }
