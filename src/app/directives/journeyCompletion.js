91865d39f52727b5a3d018934302b241caf96d81
agmo <mor.agnieszka@gmail.com>
2016-02-06 17:38:03 +0100

    Wrap the Journey Completion directive

--- src/app/directives/journeyCompletion.js
+++ src/app/directives/journeyCompletion.js
@@ -1,29 +1,31 @@
-angular.module('transport')
-    .directive('journeyCompletion', function () {
-        return {
-            restrict: 'E',
-            templateUrl: 'src/app/directives/journeyCompletion.html',
-            controller: function ($scope) {
-                $scope.plannedJourneys = [
-                    {
-                        date: new Date(),
-                        busStop: 'Migowo',
-                        lineNo: '127',
-                        departure: (new Date).getTime()
-                    },
-                    {
-                        date: new Date(),
-                        busStop: 'Gdańsk Wrzeszcz',
-                        lineNo: '122',
-                        departure: (new Date).getTime()
-                    },
-                    {
-                        date: new Date(),
-                        busStop: 'Hala Olivia',
-                        lineNo: '199',
-                        departure: (new Date).getTime()
-                    }
-                ];
+(function () {
+    angular.module('transport')
+        .directive('journeyCompletion', function () {
+            return {
+                restrict: 'E',
+                templateUrl: 'src/app/directives/journeyCompletion.html',
+                controller: function ($scope) {
+                    $scope.plannedJourneys = [
+                        {
+                            date: new Date(),
+                            busStop: 'Migowo',
+                            lineNo: '127',
+                            departure: (new Date).getTime()
+                        },
+                        {
+                            date: new Date(),
+                            busStop: 'Gdańsk Wrzeszcz',
+                            lineNo: '122',
+                            departure: (new Date).getTime()
+                        },
+                        {
+                            date: new Date(),
+                            busStop: 'Hala Olivia',
+                            lineNo: '199',
+                            departure: (new Date).getTime()
+                        }
+                    ];
+                }
             }
-        }
-    });
+        });
+}());
