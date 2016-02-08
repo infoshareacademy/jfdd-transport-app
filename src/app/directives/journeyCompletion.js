5ff6a1e2c847918ad58c5f36c77ada3ec126b03a
agmo <mor.agnieszka@gmail.com>
2016-02-08 21:52:28 +0100

    Display a success message when all journey stages have been marked as complete

--- src/app/directives/journeyCompletion.js
+++ src/app/directives/journeyCompletion.js
@@ -5,6 +5,10 @@
                 restrict: 'E',
                 templateUrl: 'src/app/directives/journeyCompletion.html',
                 controller: function ($scope) {
+                    $scope.markComplete = function (index) {
+                        $scope.plannedJourneys[index].hide = true;
+                        $scope.allStagesCompleted = $scope.plannedJourneys.every(checkIfHidden);
+                    };
                     $scope.plannedJourneys = [
                         {
                             date: new Date(),
@@ -25,6 +29,10 @@
                             departure: (new Date).getTime()
                         }
                     ];
+
+                    function checkIfHidden (value) {
+                        return value.hide;
+                    }
                 }
             }
         });
