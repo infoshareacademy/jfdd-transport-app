cf47534bebe249b02c396ff32b3f996fb8115aa9
agaSemrau <agnieszka.semrau@wp.pl>
2016-02-09 01:06:17 +0100

    try filter out stops

--- src/app/directives/journeyPlanner.js
+++ src/app/directives/journeyPlanner.js
@@ -6,7 +6,15 @@
 
             controller:  function($scope, $http) {
                 $http.get('https://isa-api.herokuapp.com/transport/stops.json').then(function(response){
-                    $scope.stops = response.data
+                    $scope.stops = response.data;
+                    $scope.myStop = $scope.stops[0].name;
+                    $scope.selected=[{name: "bajki"}];
+                    console.log( $scope.myStop )
+                    console.log( $scope.selected )
+
+                    $scope.logStop = function(selected){
+                        console.log(selected)
+                    }
                 })
             }
         }
@@ -18,7 +26,29 @@
 
                 controller: function($scope, $http) {
                    $http.get('https://isa-api.herokuapp.com/transport/lines.json').then(function(response){
-                       $scope.lines = response.data
+                       $scope.lines = response.data;
+                       $scope.myLine = $scope.lines[0].name;
+                       $scope.logLine = function(selected){
+                           console.log(selected);
+                           console.log($scope.lines);
+
+
+                              var newLines =  $scope.lines.map(function(line)
+                               {return line.stops});
+
+                             console.log(newLines);
+
+                           var stopsList = newLines.map(function(stop){
+                               return stop})
+
+
+                           console.log(stopsList);
+
+                        //$scope.finalStopsList = function(){stopsList.forEach(function(name){
+                        // console.log (name)
+                        //    })}();
+
+                       }
                    })
                 }
             }
