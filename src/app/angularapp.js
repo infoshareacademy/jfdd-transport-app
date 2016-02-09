9fe3c7010f3a3465b13cf87d78bc289786d1eeef
Wojciech Przebieracz <przebieracz@gmail.com>
2016-02-09 01:59:02 +0100

    select fav stops

--- src/app/angularapp.js
+++ src/app/angularapp.js
@@ -3,7 +3,39 @@
 
     app.controller('transportCtrl', function ($scope) {
        $scope.app = 'Jupi angular';
-    });
+        $scope.getFavStop = getFavStop;
 
+        function getFavStop(){
+            var array =  JSON.parse(localStorage.getItem("Logger")) || [];
+            var a = array.filter(function (o) {
+                if('FavStop' in o){
+                    return true;
+                }
+            } );
+            var counterObject = {};
+            var b = a.forEach(function (obj) {
+                var stopName = obj.FavStop;
+                if (counterObject[stopName] !== undefined)
+                {
+                    counterObject[stopName]++;
+                } else {
+                    counterObject[stopName] = 1;
+                }
+            });
+
+            for (busStopProperty in counterObject) {
+                var currCount = counterObject[busStopProperty];
+
+                if (currCount > highestCount)
+                var highestCount = counterObject[busStopProperty];
+
+                var maxBus;
+                var maxCount;
+                var currBus = busStopProperty;
+                var currCount = counterObject[busStopProperty];
+            }
+
+        };
+    });
 
 })();
