7f21d956601bff237acebcf21e88b08f28c8cce8
anna <annokr1@gmail.com>
2016-02-10 16:33:32 +0100

    fix controller journeyPlannerCtrl

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ $(function () {
     app.login.main.init();
 
 
-    app.pickYourStops.model.user.init(name);
+    /*app.pickYourStops.model.user.init(name);
     app.state.init();
     app.yourStopInfo.main.init();
     app.lineStats.main.init();
     $('.popup').hide();
-
+*/
 
 });
 
