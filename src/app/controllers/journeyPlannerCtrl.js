3d4209800313c47e74853fe019fdef9caa479c84
agmo <mor.agnieszka@gmail.com>
2016-02-11 16:30:34 +0100

    Implement new data structure

--- src/app/controllers/journeyPlannerCtrl.js
+++ src/app/controllers/journeyPlannerCtrl.js
@@ -5,22 +5,25 @@
 
         $scope.plannedJourneys = [
             {
-                date: new Date(),
-                busStop: 'Migowo',
-                lineNo: '127',
-                departure: (new Date).getTime()
-            },
-            {
-                date: new Date(),
-                busStop: 'Gdańsk Wrzeszcz',
-                lineNo: '122',
-                departure: (new Date).getTime()
-            },
-            {
-                date: new Date(),
-                busStop: 'Hala Olivia',
-                lineNo: '199',
-                departure: (new Date).getTime()
+                name: "Wycieczka 1",
+                date: '2016-02-27',
+                stages: [
+                    {
+                        stop: 'Migowo',
+                        line: 127,
+                        departure: (new Date).getTime()
+                    },
+                    {
+                        stop: 'Gdańsk Wrzeszcz',
+                        line: 127,
+                        departure: (new Date).getTime()
+                    },
+                    {
+                        stop: 'Hala Olivia',
+                        line: 127,
+                        departure: (new Date).getTime()
+                    }
+                ]
             }
         ];
     });
