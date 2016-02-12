899cdcd5488220fe6d2a4ebe9c94eb44223840f6
agmo <mor.agnieszka@gmail.com>
2016-02-12 15:45:19 +0100

    Mark all stages complete for multiple journeys

--- src/app/directives/journeyCompletion.js
+++ src/app/directives/journeyCompletion.js
@@ -9,12 +9,32 @@
                     $scope.journeyData = $scope.trips;
 
                     $scope.markComplete = function (index) {
-                        var activeJourney = $scope.journeyData.filter(function (element) {
+                        //var activeJourney = $scope.journeyData.filter(function (element) {
+                        //    return element.active;
+                        //});
+                        //
+                        //activeJourney[0].stages[index].hide = true;
+                        //
+                        $scope.journeyData
+                            .filter(function (element) {
                             return element.active;
-                        });
+                        })[0].stages[index].hide = true;
 
-                        activeJourney[0].stages[index].hide = true;
-                        $scope.allStagesCompleted = activeJourney[0].stages.every(checkIfHidden);
+                        $scope.allStagesCompleted = $scope.journeyData.filter(function (element) {
+                                return element.active;
+                            })[0].stages.every(checkIfHidden);
+
+
+
+                        if($scope.allStagesCompleted) {
+
+                            //$scope.journeyData.active = false;
+
+                            $scope.journeyData.forEach(function (journey) {
+                                journey.active = false;
+                            });
+                            console.log('$scope.journeyData', $scope.journeyData);
+                        }
                     };
 
                     function checkIfHidden (value) {
