b0698d604d701fea1be8e66da0b336e806616842
Wojciech Przebieracz <przebieracz@gmail.com>
2016-02-14 16:01:40 +0100

    add faved stops

--- src/app/angularapp.js
+++ src/app/angularapp.js
@@ -1,142 +1,138 @@
-(function(){
+(function () {
     var app = angular.module('transport', ['ngMaterial', 'ngMessages']);
 
-    app
-        .controller('transportCtrl', function ($scope, $interval) {
+    app.controller('transportCtrl', function ($scope, $interval) {
 ////
 
-            $scope.getFavStop = getFavStop;
-            $scope.favStop = "test";
-            $interval(function () {
-                $scope.favStop = getFavStop();
-            },10);
+        $scope.getFavStop = getFavStop;
+        $scope.favStop = "test";
+        $interval(function () {
+            $scope.favStop = getFavStop();
+        }, 10);
 
-            function getFavStop(){
-                var loggerDataArray =  JSON.parse(localStorage.getItem("Logger")) || [];
-                var favStopsArray = loggerDataArray.filter(function (o) {
-                    if('FavStop' in o){
-                        return true;
-                    }
-                } );
-                var counterForFavedStopsObject = {};
-                favStopsArray.forEach(function (obj) {
-                    var stopName = obj.FavStop;
-                    if (counterForFavedStopsObject[stopName] !== undefined)
-                    {
-                        counterForFavedStopsObject[stopName]++;
-                    } else {
-                        counterForFavedStopsObject[stopName] = 1;
-                    }
-                });
-
-                var maximumFavedCount = 0;
-                var mostFavedStop;
-                for (var busStopProperty in counterForFavedStopsObject) {
-                    if (counterForFavedStopsObject[busStopProperty]>maximumFavedCount) {
-                        maximumFavedCount = counterForFavedStopsObject[busStopProperty];
-                        mostFavedStop = busStopProperty;
-                    }
-
-
+        function getFavStop() {
+            var loggerDataArray = JSON.parse(localStorage.getItem("Logger")) || [];
+            var favStopsArray = loggerDataArray.filter(function (o) {
+                if ('FavStop' in o) {
+                    return true;
                 }
-                return 'Przystanek: ' + mostFavedStop + '. Wybrany: ' + maximumFavedCount + ' razy.';
-            };
+            });
+            var counterForFavedStopsObject = {};
+            favStopsArray.forEach(function (obj) {
+                var stopName = obj.FavStop;
+                if (counterForFavedStopsObject[stopName] !== undefined) {
+                    counterForFavedStopsObject[stopName]++;
+                } else {
+                    counterForFavedStopsObject[stopName] = 1;
+                }
+            });
+
+            var maximumFavedCount = 0;
+            var mostFavedStop = "Brak danych";
+            for (var busStopProperty in counterForFavedStopsObject) {
+                if (counterForFavedStopsObject[busStopProperty] > maximumFavedCount) {
+                    maximumFavedCount = counterForFavedStopsObject[busStopProperty];
+                    mostFavedStop = busStopProperty;
+                }
 
 
-            //////
-            var currentTrip;
+            }
+            return 'Przystanek: ' + mostFavedStop + '. Wybrany: ' + maximumFavedCount + ' razy.';
+        }
 
-            $scope.trips = [];
+        var currentTrip;
 
-            $scope.startNewTrip = function (selectedDate) {
-                currentTrip = {};
-                currentTrip.date = selectedDate;
-                $scope.trips.push(currentTrip);
+        $scope.trips = [];
 
-            };
+        $scope.startNewTrip = function (selectedDate) {
+            currentTrip = {};
+            currentTrip.date = selectedDate;
+            $scope.trips.push(currentTrip);
 
-            $scope.flag = false;
+        };
 
-            $scope.changeState = function () {
-                $scope.flag = true;
-            };
+        $scope.flag = false;
 
-            $scope.addStageToCurrentTrip = function (stop, line, departureTime) {
+        $scope.changeState = function () {
+            $scope.flag = true;
+        };
 
-                $scope.hideValue = true;
-                currentTrip.stages = currentTrip.stages || [];
-                currentTrip.stages.push({
-                    stop: stop,
-                    line: line,
-                    departureTime: departureTime
-                });
-            };
+        $scope.addStageToCurrentTrip = function (stop, line, departureTime) {
 
-            //$scope.deleteJourney = function () {
-            //    console.log('Not implemented yet');
-            //};
+            $scope.hideValue = true;
+            currentTrip.stages = currentTrip.stages || [];
+            currentTrip.stages.push({
+                stop: stop,
+                line: line,
+                departureTime: departureTime
+            });
+        };
 
-            $scope.addStop = function (selected) {
+        //$scope.deleteJourney = function () {
+        //    console.log('Not implemented yet');
+        //};
 
-                $scope.accumulator = [];
+        $scope.addStop = function (selected) {
 
-                $scope.filteredLines = $scope.lines.map(function (line) {
+            $scope.accumulator = [];
 
-                    line.stops.forEach(function (stops) {
-                        for (var name in stops) {
-                            if (stops.name == selected) {
-                                $scope.accumulator.push(line);
+            $scope.filteredLines = $scope.lines.map(function (line) {
 
-                            }
+                line.stops.forEach(function (stops) {
+                    for (var name in stops) {
+                        if (stops.name == selected) {
+                            $scope.accumulator.push(line);
+
                         }
-                    });
-                });
-                $scope.uniqueLines = [];
-                $.each($scope.accumulator, function (i, el) {
-                    if ($.inArray(el, $scope.uniqueLines) === -1) $scope.uniqueLines.push(el);
-                });
-            }
-
-            $scope.addLine = function(selected) {
-
-                var toHHMM = function (seconds_parameter) {
-                    var sec_num = parseInt(seconds_parameter, 10);  // don't forget the radix in the second param
-                    var hours = Math.floor(sec_num / 3600);
-                    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-
-                    if (hours < 10) {
-                        hours = "0" + hours;
                     }
-                    if (minutes < 10) {
-                        minutes = "0" + minutes;
-                    }
-                    var time = hours + ':' + minutes;
-                    return time;
-                };
-
-                var matchingStopArrayIndex = selected.stops.indexOf($scope.myStop.name);
-                var sumPreviousAndCurrentElementForReduce = function(a, b) {
-                        return a + b;
-                    };
-                var timeFromStartingStopToGivenInSeconds = selected.dTimes.slice(0, matchingStopArrayIndex + 1).reduce(sumPreviousAndCurrentElementForReduce, 0);
-
-                var singleLineDepartures = selected.departures.map(function (singleDepartureTime) {
-                    var departureTimesInSeconds = singleDepartureTime.hour * 3600 + singleDepartureTime.minutes * 60 + singleDepartureTime.seconds;
-                    var departureTimeOnCurrentStop = (departureTimesInSeconds + timeFromStartingStopToGivenInSeconds) % 86400; // 86400s = 24h
-                    var departureTimeOnCurrentStopHHMM = toHHMM(departureTimeOnCurrentStop);
-                    return departureTimeOnCurrentStopHHMM;
                 });
-                console.log(singleLineDepartures);
-                $scope.myTime = singleLineDepartures;
+            });
+            $scope.uniqueLines = [];
+            $.each($scope.accumulator, function (i, el) {
+                if ($.inArray(el, $scope.uniqueLines) === -1) $scope.uniqueLines.push(el);
+            });
+        }
 
-                $scope.times = $scope.myTime[0]
-            }
+        $scope.addLine = function (selected) {
 
-            $scope.addHour = function(selected){
+            var toHHMM = function (seconds_parameter) {
+                var sec_num = parseInt(seconds_parameter, 10);  // don't forget the radix in the second param
+                var hours = Math.floor(sec_num / 3600);
+                var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
 
-                console.log('wartosc ' + selected)
+                if (hours < 10) {
+                    hours = "0" + hours;
+                }
+                if (minutes < 10) {
+                    minutes = "0" + minutes;
+                }
+                var time = hours + ':' + minutes;
+                return time;
+            };
 
-                $scope.mySelectedTime = selected
-            }
-        })
+            var matchingStopArrayIndex = selected.stops.indexOf($scope.myStop.name);
+            var sumPreviousAndCurrentElementForReduce = function (a, b) {
+                return a + b;
+            };
+            var timeFromStartingStopToGivenInSeconds = selected.dTimes.slice(0, matchingStopArrayIndex + 1).reduce(sumPreviousAndCurrentElementForReduce, 0);
+
+            var singleLineDepartures = selected.departures.map(function (singleDepartureTime) {
+                var departureTimesInSeconds = singleDepartureTime.hour * 3600 + singleDepartureTime.minutes * 60 + singleDepartureTime.seconds;
+                var departureTimeOnCurrentStop = (departureTimesInSeconds + timeFromStartingStopToGivenInSeconds) % 86400; // 86400s = 24h
+                var departureTimeOnCurrentStopHHMM = toHHMM(departureTimeOnCurrentStop);
+                return departureTimeOnCurrentStopHHMM;
+            });
+            console.log(singleLineDepartures);
+            $scope.myTime = singleLineDepartures;
+
+            $scope.times = $scope.myTime[0]
+        }
+
+        $scope.addHour = function (selected) {
+
+            console.log('wartosc ' + selected)
+
+            $scope.mySelectedTime = selected
+        }
+    })
 })();
