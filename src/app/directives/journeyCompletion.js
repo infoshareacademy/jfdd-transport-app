7698da5fff17de2b7d57b36ef5a1f3e84e431358
agmo <mor.agnieszka@gmail.com>
2016-02-14 11:37:04 +0100

    Remove obsolete code

--- src/app/directives/journeyCompletion.js
+++ src/app/directives/journeyCompletion.js
@@ -9,12 +9,6 @@
                     $scope.journeyData = $scope.trips;
 
                     $scope.markComplete = function (index) {
-                        //var activeJourney = $scope.journeyData.filter(function (element) {
-                        //    return element.active;
-                        //});
-                        //
-                        //activeJourney[0].stages[index].hide = true;
-                        //
                         $scope.journeyData
                             .filter(function (element) {
                             return element.active;
@@ -27,13 +21,9 @@
 
 
                         if($scope.allStagesCompleted) {
-
-                            //$scope.journeyData.active = false;
-
                             $scope.journeyData.forEach(function (journey) {
                                 journey.active = false;
                             });
-                            console.log('$scope.journeyData', $scope.journeyData);
                         }
                     };
 
