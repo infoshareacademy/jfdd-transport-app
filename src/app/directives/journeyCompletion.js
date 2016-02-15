2c2488dd516df9f90afa4a2b04f4533acc8da549
agmo <mor.agnieszka@gmail.com>
2016-02-15 22:01:33 +0100

    Delete commented code

--- src/app/directives/journeyCompletion.js
+++ src/app/directives/journeyCompletion.js
@@ -15,10 +15,7 @@
                             })[0];
 
                         currentJourney.stages[index].hide = true;
-                      //delete currentJourney.stages[index];
-                      //  var buttonToBeRemoved = angular.element(document.querySelector( '#buttonCompleted' + index).ownerDocument.activeElement);
-                      //  buttonToBeRemoved.remove();
-
+                     
                         var allStagesCompleted = currentJourney.stages.every(checkIfHidden);
                         if (allStagesCompleted) {
                             currentJourney.journeyCompleted = true;
